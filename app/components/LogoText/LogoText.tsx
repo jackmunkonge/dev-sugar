be74fab062f1be0a1afb73ace29bf4a75cf35ac0
Jack Munkonge <jackmunkonge@gmail.com>
2022-07-02 01:18:57 +0100

    Add LogoText

---
+++ app/components/LogoText/LogoText.tsx
@@ -0,0 +1,152 @@
+import React from "react";
+
+type LogoTextProps = {
+  width?: number;
+  height?: number;
+};
+
+const LogoText: React.FC<LogoTextProps> = ({ width = 225, height = 28 }) => (
+  <svg width={width} height={height} viewBox="0 0 450 56">
+    <path d="M277 48h4v4h-4v-4Z" fill="#F1789C" />
+    <path d="M273 48h4v4h-4v-4Z" fill="#F880A4" />
+    <path d="M269 48h4v4h-4v-4Z" fill="#EF497A" />
+    <path d="M265 48h4v4h-4v-4Z" fill="#FF598B" />
+    <path d="M261 48h4v4h-4v-4Z" fill="#EE4075" />
+    <path d="M257 48h4v4h-4v-4Z" fill="#DB4572" />
+    <path d="M279 44h4v4h-4v-4Z" fill="#FF9DBB" />
+    <path d="M283 44h4v4h-4v-4Z" fill="#F76C96" />
+    <path d="M287 44h4v4h-4v-4Z" fill="#FF8FB1" />
+    <path d="M251 44h4v4h-4v-4Z" fill="#FF749E" />
+    <path d="M247 44h4v4h-4v-4Z" fill="#FF81A7" />
+    <path d="M253 48h4v4h-4v-4Z" fill="#EA7A9B" />
+    <path d="M255 44h4v4h-4v-4Z" fill="#FFE8D2" />
+    <path d="M259 44h4v4h-4v-4Z" fill="#FFE0BB" />
+    <path d="M263 44h4v4h-4v-4Z" fill="#FEF0D9" />
+    <path d="M267 44h4v4h-4v-4Z" fill="#FFF2D1" />
+    <path d="M271 44h4v4h-4v-4Z" fill="#FFE4CB" />
+    <path d="M275 44h4v4h-4v-4Z" fill="#FED8B4" />
+    <path d="M275 40h4v4h-4v-4Z" fill="#FFEDDC" />
+    <path d="M279 40h4v4h-4v-4Z" fill="#FFE8C2" />
+    <path d="M283 40h4v4h-4v-4ZM285 36h4v4h-4v-4ZM245 36h4v4h-4v-4Z" fill="#FFECCC" />
+    <path d="M287 40h4v4h-4v-4Z" fill="#FED8B4" />
+    <path d="M271 40h4v4h-4v-4Z" fill="#FFF5EB" />
+    <path d="M267 40h4v4h-4v-4Z" fill="#FFEFDF" />
+    <path d="M263 40h4v4h-4v-4Z" fill="#FEF1E2" />
+    <path d="M259 40h4v4h-4v-4Z" fill="#FFEBD9" />
+    <path d="M255 40h4v4h-4v-4Z" fill="#FFDDBD" />
+    <path d="M251 40h4v4h-4v-4Z" fill="#FED8B4" />
+    <path d="M247 40h4v4h-4v-4Z" fill="#FFE8D2" />
+    <path d="M249 36h4v4h-4v-4Z" fill="#FD87AA" />
+    <path d="M253 36h4v4h-4v-4Z" fill="#FC2364" />
+    <path d="M249 32h4v4h-4v-4Z" fill="#FDA3BE" />
+    <path d="M253 32h4v4h-4v-4Z" fill="#D0416C" />
+    <path d="M245 32h4v4h-4v-4Z" fill="#FE81A7" />
+    <path d="M257 36h4v4h-4v-4Z" fill="#F25584" />
+    <path d="M261 36h4v4h-4v-4Z" fill="#FE81A7" />
+    <path d="M265 36h4v4h-4v-4Z" fill="#FEB7CC" />
+    <path d="M269 36h4v4h-4v-4Z" fill="#F0618C" />
+    <path d="M265 40h4v4h-4v-4Z" fill="#FB87AA" />
+    <path d="M269 40h4v4h-4v-4Z" fill="#F95183" />
+    <path d="M261 40h4v4h-4v-4Z" fill="#F0618C" />
+    <path d="M273 36h4v4h-4v-4Z" fill="#FF719C" />
+    <path d="M277 36h4v4h-4v-4Z" fill="#FFAAC4" />
+    <path d="M281 36h4v4h-4v-4Z" fill="#F87FA3" />
+    <path d="M281 32h4v4h-4v-4Z" fill="#FC6894" />
+    <path d="M277 32h4v4h-4v-4Z" fill="#EB8AA7" />
+    <path d="M285 32h4v4h-4v-4Z" fill="#E94375" />
+    <path d="M257 32h4v4h-4v-4Z" fill="#FFEAD1" />
+    <path d="M261 32h4v4h-4v-4Z" fill="#FFEBD9" />
+    <path d="M265 32h4v4h-4v-4Z" fill="#FFF3E8" />
+    <path d="M269 32h4v4h-4v-4Z" fill="#FFEBD9" />
+    <path d="M273 32h4v4h-4v-4Z" fill="#FFE0C4" />
+    <path d="M273 28h4v4h-4v-4Z" fill="#FFEBD9" />
+    <path d="M277 28h4v4h-4v-4Z" fill="#FFD5AE" />
+    <path d="M281 28h4v4h-4v-4Z" fill="#FFDCB4" />
+    <path d="M285 28h4v4h-4v-4Z" fill="#FFE6C9" />
+    <path d="M269 28h4v4h-4v-4Z" fill="#F9D8BC" />
+    <path d="M265 28h4v4h-4v-4Z" fill="#FFEBD9" />
+    <path d="M261 28h4v4h-4v-4Z" fill="#FFE4CB" />
+    <path d="M257 28h4v4h-4v-4Z" fill="#FFEBD9" />
+    <path d="M253 28h4v4h-4v-4Z" fill="#FFDDBE" />
+    <path d="M249 28h4v4h-4v-4Z" fill="#FFE5CE" />
+    <path d="M249 24h4v4h-4v-4Z" fill="#FFE9D5" />
+    <path d="M253 24h4v4h-4v-4Z" fill="#FFF7EF" />
+    <path d="M245 24h4v4h-4v-4Z" fill="#FFE6D0" />
+    <path d="M277 24h4v4h-4v-4Z" fill="#FFF7EF" />
+    <path d="M245 28h4v4h-4v-4Z" fill="#FFE0BC" />
+    <path d="M281 24h4v4h-4v-4Z" fill="#FFEBD9" />
+    <path d="M285 24h4v4h-4v-4Z" fill="#FFE5CE" />
+    <path d="M249 16h4v4h-4v-4Z" fill="#F188A7" />
+    <path d="M251 16h4v4h-4v-4Z" fill="#FC5E8E" />
+    <path d="M257 20h4v4h-4v-4Z" fill="#FB6794" />
+    <path d="M253 20h4v4h-4v-4Z" fill="#FFAFC7" />
+    <path d="M249 20h4v4h-4v-4Z" fill="#F72D6A" />
+    <path d="M253 24h4v4h-4v-4Z" fill="#FF759E" />
+    <path d="M257 24h4v4h-4v-4Z" fill="#FF97B6" />
+    <path d="M261 24h4v4h-4v-4Z" fill="#FF7BA3" />
+    <path d="M265 24h4v4h-4v-4Z" fill="#F34A7D" />
+    <path d="M269 24h4v4h-4v-4Z" fill="#CB406A" />
+    <path d="M273 24h4v4h-4v-4Z" fill="#FF9EBB" />
+    <path d="M277 20h4v4h-4v-4Z" fill="#FF7BA3" />
+    <path d="M281 20h4v4h-4v-4Z" fill="#FE90B1" />
+    <path d="M285 20h4v4h-4v-4Z" fill="#ED215E" />
+    <path d="M245 20h4v4h-4v-4Z" fill="#FD6593" />
+    <path d="M255 16h4v4h-4v-4Z" fill="#FFE8CC" />
+    <path d="M257 16h4v4h-4v-4Z" fill="#FFE2BF" />
+    <path d="M253 12h4v4h-4v-4Z" fill="#FFE5C6" />
+    <path d="M257 12h4v4h-4v-4Z" fill="#FFD6A7" />
+    <path d="M251 8h4v4h-4V8Z" fill="#FEF1E2" />
+    <path d="M261 20h4v4h-4v-4Z" fill="#FCD5B9" />
+    <path d="M265 20h4v4h-4v-4Z" fill="#FFEEC2" />
+    <path d="M269 20h4v4h-4v-4ZM273 20h4v4h-4v-4Z" fill="#FEF1E2" />
+    <path d="M261 16h4v4h-4v-4Z" fill="#FFE3C1" />
+    <path d="M265 16h4v4h-4v-4Z" fill="#FEF1E2" />
+    <path d="M269 16h4v4h-4v-4Z" fill="#FFE8CD" />
+    <path d="M273 16h4v4h-4v-4Z" fill="#FBDCB9" />
+    <path d="M277 16h4v4h-4v-4Z" fill="#FFEBD3" />
+    <path d="M281 16h4v4h-4v-4Z" fill="#FFDDB5" />
+    <path d="M261 12h4v4h-4v-4Z" fill="#F6D8B4" />
+    <path d="M257 8h4v4h-4V8Z" fill="#F47FA3" />
+    <path d="M255 8h4v4h-4V8Z" fill="#FF94B4" />
+    <path d="M257 4h4v4h-4V4Z" fill="#D63B69" />
+    <path d="M261 8h4v4h-4V8Z" fill="#FF8AAD" />
+    <path d="M265 12h4v4h-4v-4Z" fill="#E03D6E" />
+    <path d="M269 12h4v4h-4v-4Z" fill="#FF9DBB" />
+    <path d="M273 12h4v4h-4v-4Z" fill="#EA4879" />
+    <path d="M277 12h4v4h-4v-4Z" fill="#D62D60" />
+    <path d="M263 10h4v4h-4v-4Z" fill="#EF8DAB" />
+    <path d="M267 10h4v4h-4v-4Z" fill="#EF6890" />
+    <path d="M269 10h4v4h-4v-4Z" fill="#FF87AB" />
+    <path d="M260 4h4v4h-4V4Z" fill="#FFAEC7" />
+    <path d="M261 0h4v4h-4V0Z" fill="#FF9EBB" />
+    <path d="M273 8h4v4h-4V8Z" fill="#FFD8B6" />
+    <path d="M269 6h4v4h-4V6Z" fill="#ECB381" />
+    <path d="M275 10h4v2h-4v-2Z" fill="#FFDFC2" />
+    <path d="M266 2h4v4h-4V2Z" fill="#A50303" />
+    <path d="M265 2h4v4h-4V2Z" fill="#B90000" />
+    <path d="M264 4h4v4h-4V4Z" fill="#D02D2D" />
+    <path d="M265 6h4v4h-4V6Z" fill="#B60202" />
+    <path d="M267 2h4v4h-4V2Z" fill="#A01717" />
+    <path d="M268 4h4v4h-4V4Z" fill="#B11B1B" />
+    <path d="M267 6h4v4h-4V6Z" fill="#A50303" />
+    <path d="M270 1h2v2h-2V1ZM272 0h2v2h-2V0ZM243 22v28h7v6h35v-6h7V22h-8v20h-7v6h-19v-6h-7V22h-8Z" fill="#684B39" />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M0 22v34h40.86v-6.8h6.81V28.8h-6.81V22H0Zm27.24 6.8v20.4h-6.81V28.8h6.81Z"
+      fill="#684B39"
+    />
+    <path
+      d="M100.515 49.2h-27.24v-6.8h20.43v-6.8h-20.43v-6.8h27.24V22h-47.67v34h47.67v-6.8ZM139.74 56h-20.43v-6.8h-6.81v-6.8h-6.81V22h20.43v20.4h6.81V22h20.43v20.4h-6.81v6.8h-6.81V56ZM190.95 49.2V56h40.86v-6.8h6.81v-6.8h-6.81v-6.8h-20.43v-6.8h27.24V22h-40.86v6.8h-6.81v6.8h6.81v6.8h20.43v6.8h-27.24ZM303.45 56h34.05v-6.8h6.81V35.6h-20.43v6.8h6.81v6.8h-13.62V28.8h20.43V22h-34.05v6.8h-6.81v20.4h6.81V56Z"
+      fill="#684B39"
+    />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M397.155 56h-20.43v-6.8h-6.81V56h-20.43V28.8h6.81V22h34.05v6.8h6.81V56Zm-20.43-27.2h-6.81v13.6h6.81V28.8ZM429.57 56H450v-6.8h-6.81v-6.8H450V28.8h-6.81V22h-40.859v34h20.429v-6.8h6.81V56Zm-6.81-27.2h6.81v13.6h-6.81V28.8Z"
+      fill="#684B39"
+    />
+  </svg>
+);
+
+export default LogoText;
