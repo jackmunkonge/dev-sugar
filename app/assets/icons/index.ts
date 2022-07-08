19d5228560ac99245767ab07a19affc2e2d29f60
Jack Munkonge <jackmunkonge@gmail.com>
2022-07-08 10:20:19 +0100

    Create loading spinner

--- app/assets/icons/index.ts
+++ app/assets/icons/index.ts
@@ -1,5 +1,6 @@
-export { default as Hamburger } from "./Hamburger/Hamburger";
-export { default as Lightbulb } from "./Lightbulb/Lightbulb";
 export { default as Cross } from "./Cross/Cross";
+export { default as Hamburger } from "./Hamburger/Hamburger";
 export { default as Heart } from "./Heart/Heart";
+export { default as Lightbulb } from "./Lightbulb/Lightbulb";
 export { default as Scroll } from "./Scroll/Scroll";
+export { default as Spinner } from "./Spinner/Spinner";
