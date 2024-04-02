943525cedc78fc26aa78d0861e5c49e8ddf13adf
Jack Munkonge <jackmunkonge@gmail.com>
2024-04-02 16:41:26 +0100

    Total links constant

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 import { Body1, Headline5 } from '@components/Typography';
 import { CategorySearch } from '@components/index';
+import { TOTAL_LINKS } from '@utils/globalConstants';
 
 export default function Index() {
   const SiteIntro = () => (
     <header>
       <Headline5 className="text-center">the best learning resources for software devs</Headline5>
       <div className="flex flex-row items-center justify-center space-x-2">
-        {/* TODO: Add real links counter */}
-        <Headline5 className="text-primary">26,344</Headline5>
+        <Headline5 className="text-primary">{TOTAL_LINKS.toLocaleString()}</Headline5>
         <span>
           <Body1>links and counting!</Body1>
         </span>
--- src/utils/globalConstants.ts
+++ src/utils/globalConstants.ts
@@ -4,3 +4,5 @@ import resolveConfig from 'tailwindcss/resolveConfig';
 export const { theme } = resolveConfig(tailwindConfig) as any;
 
 // export const CONTENT_HEIGHT = 'h-[calc(100vh_-_(4rem_+_2px_+_2rem))]'; // Full viewport height -navbar -navbarBorder -footer
+
+export const TOTAL_LINKS = 26_344;
